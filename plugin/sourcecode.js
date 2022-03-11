ea7567909f4bf9d7561867e092ca2cf38173605e
Menarilah dan terus tertawa <83388494+LasDrak@users.noreply.github.com>
2022-03-11 18:13:52 +0800

    Update plugins/sourcecode.js

--- plugin/sourcecode.js
+++ plugin/sourcecode.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fet
 *────────「 SCRIPT 」 ────────*
 Script Ori: https://github.com/Kangsad01/wabot-tsb01
 
-Script Recode By Pilar: https://github.com/PilarV2/Zeus-BotzV4
-`.trim(), '© ZeusBotz', 'Thanks', '.tqto')
+Script Recode By Pilar: https://github.com/LasDrak/ElainaXyz
+`.trim(), 'Elainaエライナ', 'Thanks', '.tqto')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
